8f049b5f649dbfee7b23cce30c12c05ebb377b86
Jesus Reyes <jesusreyes.personal@gmail.com>
2015-09-28 08:30:11 -0500

    Se crea openerp__.py con datos basicos.

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'RitSoft Theme',
+  'description': 'Tema oficial de RitSoft',
+  'version':'1.0',
+  'author':'Jesus Reyes <jesusreyes.personal@gmail.com>',
+
+  'data': [
+  ],
+  'category': 'Theme/Personal',
+  'depends': ['website', 'website_less'],
+}
